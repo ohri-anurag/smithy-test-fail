6dfa95f120961a4224b71b0362fad26498b88857
Anurag Ohri <anuragohri92@gmail.com>
2022-09-12 13:46:26 +0530

    Added a build script that logs time when it runs

--- release.nix
+++ release.nix
@@ -1,1 +1,22 @@
-derivation { name = "test-fail"; builder = "doesn't exist"; system = builtins.currentSystem; }
+let
+  nixpkgs =
+    let
+      src = builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/30d3d79b7d3607d56546dd2a6b49e156ba0ec634.tar.gz";
+        sha256 = "0x5j9q1vi00c6kavnjlrwl3yy1xs60c34pkygm49dld2sgws7n0a";
+      };
+    in import src {};
+  nixpkgsDiamond = import <nixpkgs> {};
+  lol = "pay no attention to this let-binding!!1";
+in
+nixpkgs.stdenv.mkDerivation {
+  name = "test-repo-smithy";
+  src = ./data;
+  installPhase = ''
+    mkdir -p $out
+    cp data.bin $out/
+    echo "${nixpkgsDiamond.stdenv.system}" > $out/system
+    now="$(date)"
+    printf "Current date and time %s\n" "$now"
+  '';
+}
